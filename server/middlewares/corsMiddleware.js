5d4187ac11cd688cf9f61b11a1d01c869cca7d9e
Ubuntu <ubuntu@ip-172-31-48-9.ec2.internal>
2021-03-23 18:58:42 +0000

    Update cors middleware

--- server/middlewares/corsMiddleware.js
+++ server/middlewares/corsMiddleware.js
@@ -3,7 +3,7 @@
 const corsMiddleware = (req, res, next) => {
     res.header('Access-Control-Allow-Credentials', true);
     res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Access-Control-Allow-Headers, Authorization, Content-Type, Accept');
+    res.header('Access-Control-Allow-Headers', '*');
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE, PATCH');
     if (req.method === "OPTIONS") {
       return res.status(200).send("OK");
